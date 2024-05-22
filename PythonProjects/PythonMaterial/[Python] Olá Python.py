01bb67c721781f3ab163c2d40f9b90b8fe36bb3c
Raul Oliveira <168892121+DevRaulOliveira@users.noreply.github.com>
2024-05-22 06:59:50 -0300

    Delete PythonProjects directory

--- PythonProjects/PythonMaterial/[Python] Olá Python.py
+++
@@ -1,4 +0,0 @@
-# Desafio 01 - Udemy
-# Para este desafio, quero que voce crie um script que imprima a frase "Olá, mundo Python!" no console.
-# Comando PRINT é o comando utilizado para imprimir o texto no terminal
-print("Ola, mundo Python!")
